672f57dcdeb2d6d1be4007783e4b105b16716253
Olivier Duhart <olivier.duhart@sage.com>
2021-12-16 17:58:01 +0100

    dimension 2

--- ExpirationalMultiDimensionalDictionary/Program.cs
+++ ExpirationalMultiDimensionalDictionary/Program.cs
@@ -7,17 +7,64 @@ namespace ExpirationalMultiDimensionalDictionary
     {
         static void Main(string[] args)
         {
-            var dic = new MultiDimensionalDictionary<string, string>(TimeSpan.FromSeconds(3));
-            dic.Put("x","y");
-            var c = dic.ContainsKey("x");
+            // var dic = new MultiDimensionalDictionary<string, string>(TimeSpan.FromSeconds(3));
+            // dic.Put("x","y");
+            // var c = dic.ContainsKey("x");
+            // if (c)
+            // {
+            //     Thread.Sleep(4000);
+            //     c = dic.ContainsKey("x");
+            //     if (!c)
+            //     {
+            //         Console.WriteLine("K1 OK"); 
+            //     }
+            // }
+            // else
+            // {
+            //     Console.WriteLine("something's wrong");
+            // }
+
+            var dic2 = new MultiDimensionalDictionary<string, string, string>(TimeSpan.FromSeconds(6),
+                TimeSpan.FromSeconds(3));
+            
+            dic2.Put("x","y","z");
+            var c = dic2.ContainsKey("x", "y");
             if (c)
             {
+                Console.WriteLine("step1 OK");
                 Thread.Sleep(4000);
-                c = dic.ContainsKey("x");
+                c = dic2.ContainsKey("x");
+                if (c)
+                {
+                    Console.WriteLine("step2 OK");
+                    c = dic2.ContainsKey("x", "y");
+                    if (!c)
+                    {
+                        Console.WriteLine("step3 OK");
+                        Thread.Sleep(2000);
+                        c = dic2.ContainsKey("x");
+                        if (!c)
+                        {
+                            Console.WriteLine("K2 OK");
+                        }
+                        else
+                        {
+                            Console.WriteLine("final step KO");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("step3 KO");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("step2 KO");
+                }
             }
             else
             {
-                Console.WriteLine("something's wrong");
+                Console.WriteLine("step1 KO");
             }
         }
     }
