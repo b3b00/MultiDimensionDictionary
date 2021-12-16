ae510bbcb1252a38ea5314b525dcbb3a6c037350
Olivier Duhart <olivier.duhart@sage.com>
2021-12-16 16:43:07 +0100

    initial

---
+++ ExpirationalMultiDimensionalDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace ExpirationalMultiDimensionalDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dic = new MultiDimensionalDictionary<string, string>(TimeSpan.FromSeconds(3));
+            dic.Put("x","y");
+            var c = dic.ContainsKey("x");
+            if (c)
+            {
+                Thread.Sleep(4000);
+                c = dic.ContainsKey("x");
+            }
+            else
+            {
+                Console.WriteLine("something's wrong");
+            }
+        }
+    }
+}
