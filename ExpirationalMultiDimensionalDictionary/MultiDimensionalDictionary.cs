187f5d461081c4b49bdb734593c75330dc84ed49
Olivier Duhart <olivier.duhart@sage.com>
2021-12-17 10:54:33 +0100

    dimension 3

--- ExpirationalMultiDimensionalDictionary/MultiDimensionalDictionary.cs
+++ ExpirationalMultiDimensionalDictionary/MultiDimensionalDictionary.cs
@@ -246,225 +246,298 @@ public static class DictionaryAssertions
         }
     }
 
-    // public class MultiDimensionalDictionary<K1, K2, K3, V>
-    // {
-    //
-    //     protected ConcurrentDictionary<K1, MultiDimensionalDictionary<K2, K3, V>> Data { get; set; }
-    //
-    //     public MultiDimensionalDictionary() : base()
-    //     {
-    //         Data = new ConcurrentDictionary<K1, MultiDimensionalDictionary<K2, K3, V>>();
-    //     }
-    //
-    //     public bool ContainsKey(K1 k1) => Data.ContainsKey(k1);
-    //
-    //     public bool ContainsKey(K1 k1, K2 k2, K3 k3) => Data.ContainsKey(k1) && Data[k1].ContainsKey(k2, k3);
-    //
-    //     public List<(K1, K2, K3)> GetKeys()
-    //     {
-    //         List<(K1, K2, K3)> keys = new List<(K1, K2, K3)>();
-    //         foreach (var kvp in Data)
-    //         {
-    //             List<(K2, K3)> subkeys = kvp.Value.GetKeys();
-    //             foreach (var subkey in subkeys)
-    //             {
-    //                 keys.Add((kvp.Key, subkey.Item1, subkey.Item2));
-    //             }
-    //         }
-    //         return keys;
-    //     }
-    //
-    //     public void Put(K1 k1, K2 k2, K3 k3, V value)
-    //     {
-    //         DictionaryAssertions.AssertNotNull<K1>(k1, "k1");
-    //
-    //         DictionaryAssertions.AssertNotNull<K2>(k2, "k2");
-    //
-    //         DictionaryAssertions.AssertNotNull<K3>(k3, "k3");
-    //
-    //         DictionaryAssertions.AssertNotNull<V>(value, "value");
-    //
-    //         var secondDimentionData = new MultiDimensionalDictionary<K2, K3, V>(default,default);
-    //
-    //
-    //
-    //         if (Data.ContainsKey(k1))
-    //         {
-    //             secondDimentionData = Data[k1];
-    //         }
-    //
-    //         secondDimentionData.Put(k2, k3, value);
-    //
-    //         Data[k1] = secondDimentionData;
-    //     }
-    //
-    //     public MultiDimensionalDictionary<K2, K3, V> Get(K1 k1)
-    //     {
-    //         return Data[k1];
-    //     }
-    //
-    //     public MultiDimensionalDictionary<K3, V> Get(K1 k1, K2 k2)
-    //     {
-    //         return Data[k1].Get(k2);
-    //     }
-    //
-    //     public V Get(K1 k1, K2 k2, K3 k3)
-    //     {
-    //
-    //         DictionaryAssertions.AssertNotNull<K1>(k1, "k1");
-    //
-    //         DictionaryAssertions.AssertNotNull<K2>(k2, "k2");
-    //
-    //         DictionaryAssertions.AssertNotNull<K3>(k3, "k3");
-    //
-    //
-    //         V value = default(V);
-    //         MultiDimensionalDictionary<K2, K3, V> secondDimensionData;
-    //         if (Data.TryGetValue(k1, out secondDimensionData))
-    //         {
-    //             return secondDimensionData.Get(k2, k3);
-    //         }
-    //
-    //         throw new KeyNotFoundException();
-    //     }
-    //
-    //     public void Remove(K1 k1)
-    //     {
-    //         MultiDimensionalDictionary<K2, K3, V> ignore = null;
-    //         Data.Remove(k1, out ignore);
-    //     }
-    //
-    //     public void Remove(K1 k1, K2 k2)
-    //     {
-    //         Data[k1].Remove(k2);
-    //     }
-    //
-    //     public void Remove(K1 k1, K2 k2, K3 k3)
-    //     {
-    //         Data[k1].Get(k2).Remove(k3);
-    //     }
-    //
-    //     public void Clear()
-    //     {
-    //         Data.Clear();
-    //     }
-    // }
+    public class MultiDimensionalDictionary<K1, K2, K3, V>
+    {
+    
+        TimeSpan ExpirationSpan1;
+        TimeSpan ExpirationSpan2;
+        TimeSpan ExpirationSpan3;
+        
+        protected ConcurrentDictionary<K1, (DateTime date, MultiDimensionalDictionary<K2, K3, V> data)> Data { get; set; }
+    
+        public MultiDimensionalDictionary(TimeSpan expirationSpan1, TimeSpan expirationSpan2, TimeSpan expirationSpan3) : base()
+        {
+            ExpirationSpan1 = expirationSpan1;
+            ExpirationSpan2 = expirationSpan2;
+            ExpirationSpan3 = expirationSpan3;
+            Data = new ConcurrentDictionary<K1, (DateTime date, MultiDimensionalDictionary<K2, K3, V> data)>();
+        }
+    
+        
+        public void Invalidate()
+        {
+            DateTime now = DateTime.Now;
+            List<K1> toRemove = new List<K1>();
+            foreach (var entry in Data)
+            {
+                var age = now - entry.Value.date;
+                if (age > ExpirationSpan1)
+                {
+                    toRemove.Add(entry.Key);
+                }
+            }
 
-    // public class MultiDimensionalDictionary<K1, K2, K3, K4, V> : MultiDimensionalDictionary<K1, K2, MultiDimensionalDictionary<K3, K4, V>>
-    // {
-    //     protected new ConcurrentDictionary<K1, MultiDimensionalDictionary<K2, K3, K4, V>> Data { get; set; }
-    //
-    //     public MultiDimensionalDictionary() : base(default,default)
-    //     {
-    //         Data = new ConcurrentDictionary<K1, MultiDimensionalDictionary<K2, K3, K4, V>>();
-    //     }
-    //
-    //     public new bool ContainsKey(K1 k1) => Data.ContainsKey(k1);
-    //
-    //     public bool ContainsKey(K1 k1, K2 k2, K3 k3, K4 k4) => Data.ContainsKey(k1) && Data[k1].ContainsKey(k2, k3, k4);
-    //
-    //
-    //     public new List<(K1, K2, K3, K4)> GetKeys()
-    //     {
-    //         List<(K1, K2, K3, K4)> keys = new List<(K1, K2, K3, K4)>();
-    //         foreach (var kvp in Data)
-    //         {
-    //             List<(K2, K3, K4)> subkeys = kvp.Value.GetKeys();
-    //             foreach (var subkey in subkeys)
-    //             {
-    //                 keys.Add((kvp.Key, subkey.Item1, subkey.Item2, subkey.Item3));
-    //             }
-    //         }
-    //         return keys;
-    //     }
-    //
-    //     public void Put(K1 k1, K2 k2, K3 k3, K4 k4, V value)
-    //     {
-    //         DictionaryAssertions.AssertNotNull<K1>(k1, "k1");
-    //
-    //         DictionaryAssertions.AssertNotNull<K2>(k2, "k2");
-    //
-    //         DictionaryAssertions.AssertNotNull<K3>(k3, "k3");
-    //
-    //         DictionaryAssertions.AssertNotNull<K4>(k4, "k4");
-    //
-    //         DictionaryAssertions.AssertNotNull<V>(value, "value");
-    //
-    //         var secondDimentionData = new MultiDimensionalDictionary<K2, K3, K4, V>();
-    //
-    //         if (Data.ContainsKey(k1))
-    //         {
-    //             secondDimentionData = Data[k1];
-    //         }
-    //
-    //         secondDimentionData.Put(k2, k3, k4, value); ;
-    //
-    //         Data[k1] = secondDimentionData;
-    //     }
-    //
-    //
-    //     public new MultiDimensionalDictionary<K2, K3, K4, V> Get(K1 k1)
-    //     {
-    //         return Data[k1];
-    //     }
-    //
-    //     public new MultiDimensionalDictionary<K3, K4, V> Get(K1 k1, K2 k2)
-    //     {
-    //         return Data[k1].Get(k2);
-    //     }
-    //
-    //     public MultiDimensionalDictionary<K4, V> Get(K1 k1, K2 k2, K3 k3)
-    //     {
-    //         return Data[k1].Get(k2).Get(k3);
-    //     }
-    //
-    //     public V Get(K1 k1, K2 k2, K3 k3, K4 k4)
-    //     {
-    //
-    //         DictionaryAssertions.AssertNotNull<K1>(k1, "k1");
-    //
-    //         DictionaryAssertions.AssertNotNull<K2>(k2, "k2");
-    //
-    //         DictionaryAssertions.AssertNotNull<K3>(k3, "k3");
-    //
-    //         DictionaryAssertions.AssertNotNull<K4>(k4, "k4");
-    //
-    //         V value = default(V);
-    //         MultiDimensionalDictionary<K2, K3, K4, V> secondDimensionData;
-    //         if (Data.TryGetValue(k1, out secondDimensionData))
-    //         {
-    //             return secondDimensionData.Get(k2, k3, k4);
-    //         }
-    //
-    //         throw new KeyNotFoundException();
-    //     }
-    //
-    //     public new void Remove(K1 k1)
-    //     {
-    //         MultiDimensionalDictionary<K2, K3, K4, V> ignore = null;
-    //         Data.Remove(k1, out ignore);
-    //     }
-    //
-    //     public void Remove(K1 k1, K2 k2)
-    //     {
-    //         Data[k1].Remove(k2);
-    //     }
-    //
-    //     public void Remove(K1 k1, K2 k2, K3 k3)
-    //     {
-    //         Data[k1].Get(k2).Remove(k3);
-    //     }
-    //
-    //     public void Remove(K1 k1, K2 k2, K3 k3, K4 k4)
-    //     {
-    //         Data[k1].Get(k2).Get(k3).Remove(k4);
-    //     }
-    //     
-    //     public void Clear()
-    //     {
-    //         Data.Clear();
-    //     }
-    // }
+            if (toRemove.Any())
+            {
+                foreach (var k1 in toRemove)
+                {
+                    _Remove(k1);
+                }
+            }
+        }
+        
+        public void _Remove(K1 k1)
+        {
+            (DateTime date, MultiDimensionalDictionary<K2, K3, V> data) ignore;
+            Data.TryRemove(k1, out ignore);
+        }
+
+        public bool ContainsKey(K1 k1)
+        {
+            Invalidate();
+            return Data.ContainsKey(k1);
+        }
+        
+        public bool ContainsKey(K1 k1, K2 k2)
+        {
+            Invalidate();
+            return Data.ContainsKey(k1) && Data[k1].data.ContainsKey(k2);
+        }
+
+        public bool ContainsKey(K1 k1, K2 k2, K3 k3)
+        {
+            Invalidate();
+            return Data.ContainsKey(k1) && Data[k1].data.ContainsKey(k2, k3);
+        }
+
+        public List<(K1, K2, K3)> GetKeys()
+        {
+            Invalidate();
+            List<(K1, K2, K3)> keys = new List<(K1, K2, K3)>();
+            foreach (var kvp in Data)
+            {
+                List<(K2, K3)> subkeys = kvp.Value.data.GetKeys();
+                foreach (var subkey in subkeys)
+                {
+                    keys.Add((kvp.Key, subkey.Item1, subkey.Item2));
+                }
+            }
+            return keys;
+        }
+    
+        public void Put(K1 k1, K2 k2, K3 k3, V value)
+        {
+            Invalidate();
+            
+            DictionaryAssertions.AssertNotNull<K1>(k1, "k1");
+    
+            DictionaryAssertions.AssertNotNull<K2>(k2, "k2");
+    
+            DictionaryAssertions.AssertNotNull<K3>(k3, "k3");
+    
+            DictionaryAssertions.AssertNotNull<V>(value, "value");
+    
+            var secondDimentionData = new MultiDimensionalDictionary<K2, K3, V>(ExpirationSpan2,ExpirationSpan3);
+    
+    
+    
+            if (Data.ContainsKey(k1))
+            {
+                secondDimentionData = Data[k1].data;
+            }
+    
+            secondDimentionData.Put(k2, k3, value);
+    
+            Data[k1] = (DateTime.Now, secondDimentionData);
+        }
+    
+        public MultiDimensionalDictionary<K2, K3, V> Get(K1 k1)
+        {
+            Invalidate();
+            return Data[k1].data;
+        }
+    
+        public MultiDimensionalDictionary<K3, V> Get(K1 k1, K2 k2)
+        {
+            Invalidate();
+            return Data[k1].data.Get(k2);
+        }
+    
+        public V Get(K1 k1, K2 k2, K3 k3)
+        {
+            Invalidate();
+            DictionaryAssertions.AssertNotNull<K1>(k1, "k1");
+    
+            DictionaryAssertions.AssertNotNull<K2>(k2, "k2");
+    
+            DictionaryAssertions.AssertNotNull<K3>(k3, "k3");
+    
+    
+            V value = default(V);
+            (DateTime date, MultiDimensionalDictionary<K2, K3, V> data) secondDimensionData;
+            if (Data.TryGetValue(k1, out secondDimensionData))
+            {
+                return secondDimensionData.data.Get(k2, k3);
+            }
+    
+            throw new KeyNotFoundException();
+        }
+    
+        public void Remove(K1 k1)
+        {
+            Invalidate();
+            _Remove(k1);
+        }
+    
+        public void Remove(K1 k1, K2 k2)
+        {
+            Invalidate();
+            Data[k1].data.Remove(k2);
+        }
+    
+        public void Remove(K1 k1, K2 k2, K3 k3)
+        {
+            Data[k1].data.Get(k2).Remove(k3);
+        }
+    
+        public void Clear()
+        {
+            Data.Clear();
+        }
+    }
+
+    public class MultiDimensionalDictionary<K1, K2, K3, K4, V> : MultiDimensionalDictionary<K1, K2, MultiDimensionalDictionary<K3, K4, V>>
+    {
+        protected new ConcurrentDictionary<K1, (DateTime date, MultiDimensionalDictionary<K2, K3, K4, V> data)>  Data { get; set; }
+    
+        TimeSpan ExpirationSpan1;
+        TimeSpan ExpirationSpan2;
+        TimeSpan ExpirationSpan3;
+        TimeSpan ExpirationSpan4;
+        
+        public MultiDimensionalDictionary(TimeSpan expirationSpan1, TimeSpan expirationSpan2, TimeSpan expirationSpan3, TimeSpan expirationSpan4) : base(expirationSpan1, expirationSpan2)
+        {
+            Data = new ConcurrentDictionary<K1, (DateTime date, MultiDimensionalDictionary<K2, K3, K4, V> data)>();
+        }
+    
+        // public new bool ContainsKey(K1 k1) => Data.ContainsKey(k1);
+        //
+        // public bool ContainsKey(K1 k1, K2 k2, K3 k3, K4 k4)
+        // {
+        //     return Data.ContainsKey(k1) && Data[k1].data.ContainsKey(k2, k3, k4);
+        // }
+    
+    
+        public new List<(K1, K2, K3, K4)> GetKeys()
+        {
+            Invalidate();
+            List<(K1, K2, K3, K4)> keys = new List<(K1, K2, K3, K4)>();
+            foreach (var kvp in Data)
+            {
+                List<(K2, K3, K4)> subkeys = kvp.Value.data.GetKeys();
+                foreach (var subkey in subkeys)
+                {
+                    keys.Add((kvp.Key, subkey.Item1, subkey.Item2, subkey.Item3));
+                }
+            }
+            return keys;
+        }
+    
+        public void Put(K1 k1, K2 k2, K3 k3, K4 k4, V value)
+        {
+            Invalidate();
+            DictionaryAssertions.AssertNotNull<K1>(k1, "k1");
+    
+            DictionaryAssertions.AssertNotNull<K2>(k2, "k2");
+    
+            DictionaryAssertions.AssertNotNull<K3>(k3, "k3");
+    
+            DictionaryAssertions.AssertNotNull<K4>(k4, "k4");
+    
+            DictionaryAssertions.AssertNotNull<V>(value, "value");
+    
+            var secondDimentionData = new MultiDimensionalDictionary<K2, K3, K4, V>(ExpirationSpan2,ExpirationSpan3,ExpirationSpan4);
+    
+            if (Data.ContainsKey(k1))
+            {
+                secondDimentionData = Data[k1].data;
+            }
+    
+            secondDimentionData.Put(k2, k3, k4, value); ;
+    
+            Data[k1] = (DateTime.Now, secondDimentionData);
+        }
+    
+    
+        public new MultiDimensionalDictionary<K2, K3, K4, V> Get(K1 k1)
+        {
+            Invalidate();
+            return Data[k1].data;
+        }
+    
+        public new MultiDimensionalDictionary<K3, K4, V> Get(K1 k1, K2 k2)
+        {
+            Invalidate();
+            return Data[k1].data.Get(k2);
+        }
+    
+        public MultiDimensionalDictionary<K4, V> Get(K1 k1, K2 k2, K3 k3)
+        {
+            Invalidate();
+            return Data[k1].data.Get(k2).Get(k3);
+        }
+    
+        public V Get(K1 k1, K2 k2, K3 k3, K4 k4)
+        {
+            Invalidate();
+            DictionaryAssertions.AssertNotNull<K1>(k1, "k1");
+    
+            DictionaryAssertions.AssertNotNull<K2>(k2, "k2");
+    
+            DictionaryAssertions.AssertNotNull<K3>(k3, "k3");
+    
+            DictionaryAssertions.AssertNotNull<K4>(k4, "k4");
+    
+            V value = default(V);
+            (DateTime date, MultiDimensionalDictionary<K2, K3, K4, V> data) secondDimensionData;
+            if (Data.TryGetValue(k1, out secondDimensionData))
+            {
+                return secondDimensionData.data.Get(k2, k3, k4);
+            }
+    
+            throw new KeyNotFoundException();
+        }
+    
+        public new void Remove(K1 k1)
+        {
+            Invalidate();
+            (DateTime , MultiDimensionalDictionary<K2, K3, K4, V>) ignore = (default,default);
+            Data.TryRemove(k1, out ignore);
+        }
+    
+        public void Remove(K1 k1, K2 k2)
+        {
+            Invalidate();
+            Data[k1].data.Remove(k2);
+        }
+    
+        public void Remove(K1 k1, K2 k2, K3 k3)
+        {
+            Invalidate();
+            Data[k1].data.Get(k2).Remove(k3);
+        }
+    
+        public void Remove(K1 k1, K2 k2, K3 k3, K4 k4)
+        {
+            Invalidate();
+            Data[k1].data.Get(k2).Get(k3).Remove(k4);
+        }
+        
+        public void Clear()
+        {
+            Data.Clear();
+        }
+    }
 
 
     // public class MultiDimensionalDictionary<K1, K2, K3, K4, K5, V> : MultiDimensionalDictionary<K1, K2, MultiDimensionalDictionary<K3, K4, K5, V>>
