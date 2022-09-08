3e0b3f44683c64fed285412d3415cfdd9d1bdc24
b3b00 <olivier.duhart@gmail.com>
2022-09-08 15:32:32 +0200

    generator expirational dictionaries

--- DictionaryGenerator/Program.cs
+++ DictionaryGenerator/Program.cs
@@ -6,10 +6,10 @@ using DictionaryGenerator;
 for (int i = 2; i <= 10; i++)
 {
 
-    string g = Generator.Generate(i);
+    string g = ExpirationalGenerator.Generate(i);
 
     File.WriteAllText(
-        $@"C:\Users\olduh\dev\MultiDimensionDictionary\MultiDimensionDictionary\multiDimentsionDictionary\multi{i}.cs", g);
+        $@"C:\Users\olduh\dev\MultiDimensionDictionary\MultiDimensionDictionary\expirationalMultiDimensionDictionary\expirationalMulti{i}.cs", g);
 }
 
 
